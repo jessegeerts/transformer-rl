b5cd6eea57de4287a7a2e8556a2f365678240563
Jesse Geerts <jessegeerts@gmail.com>
2024-04-10 11:58:52 +0100

    change niters

--- run_reddyrep.sh
+++ run_reddyrep.sh
@@ -4,6 +4,6 @@ for K in 128 256 512 2048;
 do
 for B in 0 1 2 4;
 do
-  python reddy_replication_torch.experiment --B ${B} --K ${K} --eps 0.2 --apply_ln 1 --pB 1 --pC 0
+  python reddy_replication_torch.experiment --B ${B} --K ${K} --eps 0.2 --apply_ln 1 --pB 1 --pC 0 --niters 100000
 done
 done
